16c573835242ac394d1ba5028223e782c3dcf6f1
7a11 <ahmadmonkhor@gmail.com>
2020-09-09 02:07:25 +0300

    last commit

--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path , include
-from job import views
+from . import views
+from . import api
 
 app_name = 'job'
 
@@ -10,4 +11,14 @@ urlpatterns = [
     path('add' , views.add_job , name= 'add_job'),
     path('<str:slug>' , views.job_detail , name='job_detail'),
 
+    ## api 
+    path('api/jobs' , api.job_list_api , name= 'job_list_api'),
+    path('api/jobs/<int:id>' , api.job_detail_api , name= 'job_detail_api'),
+
+    ## class based viwes    
+    path('api/v2/jobs' , api.JobListApi.as_view() , name= 'JobListApi'),
+    path('api/v2/jobs/<int:id>' , api.JobDetail.as_view() , name= 'JobDetail'),
+
+
 ]
+
